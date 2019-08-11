5a2379e5284dd721cc5f3a3aa8f3979b3d737ae5
Andres Tiko <andres.tiko@cern.ch>
2019-08-12 01:17:46 +0300

    Initial commit

---
+++ app/server.py
@@ -0,0 +1,26 @@
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn, aiohttp, asyncio
+from io import BytesIO
+
+
+model_file_url = 'https://drive.google.com/uc?export=download&id=1mqWnwY0qlXEOcETFybrgaYyLi_RYYcAB'
+model_file_name = 'kasonhunt' #'model'
+classes = ['koer', 'hunt', 'midagi_muud']
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+@app.route('/')
+def index(request):
+    html = path/'view'/'index.html'
+    return HTMLResponse(html.open().read())
+
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv: uvicorn.run(app, host='0.0.0.0', port=8080)
+
